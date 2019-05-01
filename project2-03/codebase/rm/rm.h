61522047bae1bcde62bd61e708f059c586ae5b18
Matthew Tan <mttan08@yahoo.com>
2019-04-30 22:46:29 -0700

    Add files via upload

--- project2-03/codebase/rm/rm.h
+++ project2-03/codebase/rm/rm.h
@@ -9,52 +9,7 @@
 
 using namespace std;
 
-#define TABLE_FILE_EXTENSION ".tbl"
-
-#define TABLES_TABLE_NAME           "Tables"
-#define TABLES_TABLE_ID             1
-
-// Format for Tables table:
-// (table-id:int, table-name:varchar(50), file-name:varchar(50), system:int)
-// system will be 1 if the table is a system table, 0 otherwise
-
-#define TABLES_COL_TABLE_ID         "table-id"
-#define TABLES_COL_TABLE_NAME       "table-name"
-#define TABLES_COL_FILE_NAME        "file-name"
-//#define TABLES_COL_SYSTEM           "system"
-#define TABLES_COL_TABLE_NAME_SIZE  50
-#define TABLES_COL_FILE_NAME_SIZE   50
-
-// 1 null byte, 2 integer and 2 varchars
-#define TABLES_RECORD_DATA_SIZE 1 + 4 * INT_SIZE + TABLES_COL_TABLE_NAME_SIZE + TABLES_COL_FILE_NAME_SIZE
-
-#define COLUMNS_TABLE_NAME           "Columns"
-#define COLUMNS_TABLE_ID             2
-
-#define COLUMNS_COL_TABLE_ID         "table-id"
-#define COLUMNS_COL_COLUMN_NAME      "column-name"
-#define COLUMNS_COL_COLUMN_TYPE      "column-type"
-#define COLUMNS_COL_COLUMN_LENGTH    "column-length"
-#define COLUMNS_COL_COLUMN_POSITION  "column-position"
-#define COLUMNS_COL_COLUMN_NAME_SIZE 50
-
-// 1 null byte, 4 integer fields and a varchar
-#define COLUMNS_RECORD_DATA_SIZE 1 + 5 * INT_SIZE + COLUMNS_COL_COLUMN_NAME_SIZE
-
 # define RM_EOF (-1)  // end of a scan operator
-
-#define RM_CANNOT_MOD_SYS_TBL 1
-#define RM_NULL_COLUMN        2
-
-typedef struct IndexedAttr
-{
-    int32_t pos;
-    Attribute attr;
-    bool operator < (const IndexedAttr& t) const
-    {
-        return (pos < t.pos);
-    }
-} IndexedAttr;
 
 // RM_ScanIterator is an iteratr to go through tuples
 class RM_ScanIterator {
@@ -77,6 +32,8 @@ private:
 class RelationManager
 {
 public:
+    const int TABLES_ATTR_SIZE = 1 + 4 * 4 + 50 + 50;
+    const int COLUMNS_ATTR_SIZE = 1 + 5 * 4 + 50;
     static RelationManager* instance();
 
     RC createCatalog();
@@ -118,25 +75,29 @@ protected:
     ~RelationManager();
 
 private:
+    typedef struct IndexedAttr
+    {
+        int32_t pos;
+        Attribute attr;
+        bool operator < (const IndexedAttr& t) const
+        {
+            return (pos < t.pos);
+        }
+    } IndexedAttr;
+
     static RelationManager *_rm;
     vector<Attribute> tableDescriptor;
     vector<Attribute> columnDescriptor;
 
-    // Given a table ID and recordDescriptor, creates entries in Column table
+
     RC insertColumns(int32_t id, const vector<Attribute> &recordDescriptor);
-    // Given table ID, system flag, and table name, creates entry in Table table
+
     RC insertTable(int32_t id, int32_t system, const string &tableName);
 
-    // Get table ID of table with name tableName
     RC getTableID(const string &tableName, int32_t &tableID);
 
     RC isSystemTable(bool &system, const string &tableName);
 
-public:
-    // Extra credit work (10 points)
-    RC addAttribute(const string &tableName, const Attribute &attr);
-
-    RC dropAttribute(const string &tableName, const string &attributeName);
 };
 
 #endif
